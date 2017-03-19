3e625363afd361727ff437c38176a5a0ac712b28
Samuel Jimoh <sjimoh@my.centennialcollege.ca>
2017-03-18 21:04:07 -0400

    added page structure to my app.js

--- Scripts/app.js
+++ Scripts/app.js
@@ -11,16 +11,75 @@
 
 
 // IIFE - Immediately Invoked Function Expression
-(function (){
+(function () {
     "use strict";
 
-    switch(document.title) {
-        case "Home":
-        console.log("You are at home");
-        break;
-        case "About Me":
-        console.log("You are at the about me page");
-        break;
+    /**
+     * This function uses the document.title to switch javascript function
+     * when the page switches  
+     * 
+     * @function PageSwitcher
+     * @return {void}
+     */
+    function PageSwitcher() {
+
+        switch(document.title) {
+            case "Home":
+            Home();
+            break;
+            case "About Me":
+            About();
+            break;
+            case "Contact Me":
+            Contact();
+            break;
+            case "Projects":
+            Projects();
+            break;
+        }
     }
+
+    /**
+     * This function provides JavaScript code for the Home page
+     * 
+     * @function Home
+     * @returns {void}
+     */
+    function Home() {
+        console.log("You are at the Home Page");
+    }
+
+    /**
+     * This function provides JavaScript code for the About page
+     * 
+     * @function About
+     * @returns {void}
+     */
+    function About() {
+        console.log("You are at the About Page");
+    }
+
+    /**
+     * This function provides JavaScript code for the Contact page
+     * 
+     * @function Contact
+     * @returns {void}
+     */
+    function Contact() {
+        console.log("You are at the Contact Page");
+    }
+
+    /**
+     * This function provides JavaScript code for the Projects page
+     * 
+     * @function Projects
+     * @returns {void}
+     */
+    function Projects() {
+        console.log("You are at the Projects Page");
+    }
+
+    window.addEventListener("load", PageSwitcher);
+
 
 })();
